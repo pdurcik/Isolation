97d69220d5923528d2810ab2e36d699413bc4782
LampicJ15 <jan.lampic13@gmail.com>
2018-06-12 21:21:29 +0200

    Popravek pravilo igre

--- src/test/TestLogikaIgre.java
+++ src/test/TestLogikaIgre.java
@@ -9,23 +9,23 @@ public class TestLogikaIgre extends TestCase {
 	
 	public void testIgra() {
 		Igra igra = new Igra();
-		// Na zaèetku je na potezi beli
+		// Na zacetku je na potezi beli
 		assertEquals(Stanje.NA_POTEZI_BELI, igra.stanje());
 		// Na zacetku je stevilo Aktivnih polj enako N*N-2
 		assertEquals(igra.steviloAktivnih(), igra.N*igra.N-2);
-		//Polozaj belega igralca je na zaèetku nastavljen na (3,3) - le v testu, sicer je random!
+		//Polozaj belega igralca je na zacetku nastavljen na (3,3) - le v testu, sicer je random!
 		//assertEquals(3, igra.polozajIgralca().getX());
-		//Beli igralec ima pri teh koordinatah na zaèetku možnih 8 potez
+		//Beli igralec ima pri teh koordinatah na zacetku moznih 8 potez
 		//assertEquals(8, igra.poteze().size());
 		
 		// Naredimo eno potezo
 		igra.odigraj(igra.poteze().get(0));
 		
-		//Polozaj èrnega igralca je na zaèetku nastavljen na (0,0) - le v testu, sicer je random!
+		//Polozaj crnega igralca je na zacetku nastavljen na (0,0) - le v testu, sicer je random!
 		//assertEquals(0, igra.polozajIgralca().getX());
 		// Sedaj je na potezi CRNI in imamo aktivno polje manj
 		assertEquals(Stanje.NA_POTEZI_CRNI, igra.stanje());
-		//Po eni odigrani potezi je št. aktivnih polj N*N-3
+		//Po eni odigrani potezi je ï¿½t. aktivnih polj N*N-3
 		assertEquals(igra.steviloAktivnih(), igra.N*igra.N - 3);
 		
 	}
